4d38ffd9e8995841cfa8a05dbd925984392c89c7
Fernando Henrique Coelho Farias <nandofarias@me.com>
2018-07-02 11:19:26 -0300

    feat: NewPost component and Order By in ListPosts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import Header from './Header';
 import ListPosts from './ListPosts';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
-export default () => (
+const styles = () => ({
+  floatingButton: {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed'
+  }
+});
+export default withStyles(styles)(({ classes }) => (
   <div>
     <Header />
     <ListPosts />
+    <Button
+      component={Link}
+      to="/new"
+      variant="fab"
+      className={classes.floatingButton}
+      color="primary"
+      aria-label="add"
+    >
+      <AddIcon />
+    </Button>
   </div>
-);
+));
--- src/components/ListPosts.js
+++ src/components/ListPosts.js
@@ -4,8 +4,18 @@ import { withRouter } from 'react-router-dom';
 import { getAllPosts, getCategoryPosts } from '../actions/posts';
 import Post from './Post';
 import Typography from '@material-ui/core/Typography';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
 
 class ListPosts extends Component {
+  byVoteScore = (postA, postB) => postB.voteScore - postA.voteScore;
+  byDate = (postA, postB) => postB.timestamp - postA.timestamp;
+
+  state = {
+    sort: 'by-votes'
+  };
   componentDidMount() {
     const { match, getAllPosts, getCategoryPosts } = this.props;
     if (match.params.category) {
@@ -15,13 +25,20 @@ class ListPosts extends Component {
     }
   }
 
-  renderPosts = () => (
-    <div>
-      {this.props.posts.map(post => {
-        return <Post post={post} key={post.id} />;
-      })}
-    </div>
-  );
+  handleChangeSort = ({ target }) => this.setState({ sort: target.value });
+
+  renderPosts = () => {
+    const sortFn = this.state.sort === 'by-votes' ? this.byVoteScore : this.byDate;
+    const posts = this.props.posts.sort(sortFn);
+    console.log(posts);
+    return (
+      <div>
+        {posts.map(post => {
+          return <Post post={post} key={post.id} />;
+        })}
+      </div>
+    );
+  };
 
   renderNotFound = () => (
     <Typography paragraph variant="body2">
@@ -29,7 +46,18 @@ class ListPosts extends Component {
     </Typography>
   );
   render() {
-    return this.props.posts.length > 0 ? this.renderPosts() : this.renderNotFound();
+    return (
+      <div>
+        <FormControl>
+          <InputLabel>Sort By</InputLabel>
+          <Select value={this.state.sort} onChange={this.handleChangeSort}>
+            <MenuItem value="by-votes">Vote Score</MenuItem>
+            <MenuItem value="by-date">Date</MenuItem>
+          </Select>
+        </FormControl>
+        {this.props.posts.length > 0 ? this.renderPosts() : this.renderNotFound()}
+      </div>
+    );
   }
 }
 
