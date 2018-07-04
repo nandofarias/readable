dd80b0f1d653b9328a8a98b90fb0385f08340996
Fernando Henrique Coelho Farias <nandofarias@me.com>
2018-07-04 07:20:45 -0300

    fix: Filtering deleted comments

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -2,9 +2,11 @@ import { RECEIVE_COMMENTS } from '../actions/comments';
 export default (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_COMMENTS:
-      console.log(action.comments);
       return action.comments.length > 0
-        ? { ...state, [action.comments[0].parentId]: action.comments }
+        ? {
+            ...state,
+            [action.comments[0].parentId]: action.comments.filter(action => !action.deleted)
+          }
         : state;
     default:
       return state;
